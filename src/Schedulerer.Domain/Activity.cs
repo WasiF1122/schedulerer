0fda54817d1861cff23e9ff52e20c522dcc06b51
Evaldas Buinauskas <buinauskas.evaldas@gmail.com>
2019-12-11 21:35:11 +0200

    Initial commit

---
+++ src/Schedulerer.Domain/Activity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Schedulerer.Domain.Core;
+
+namespace Schedulerer.Domain
+{
+    public class Activity : Entity
+    {
+        public string Name { get; private set; }
+
+        private Activity () { }
+
+        private Activity (Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public static Activity Create (string name)
+        {
+            return new Activity (Guid.NewGuid (), name);
+        }
+    }
+}
